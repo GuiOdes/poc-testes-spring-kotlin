467ba6ef7af41e3c81ada46bdd61a858ca555589
Guilherme Monteiro <guilherme.montnezes@gmail.com>
2022-06-28 11:40:06 -0300

    Basic database configuration

---
+++ src/test/kotlin/com/tests/poc/PocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tests.poc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
